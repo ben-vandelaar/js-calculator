a2c2d0ffd1bdfbf02720f4d7f4e80f0377b3c0be
ben-vandelaar <benvandelaar@gmail.com>
2018-07-19 15:47:49 +1200

    laid out some objects

---
+++ calculatorJs.js
@@ -0,0 +1,41 @@
+var display = {
+
+};
+
+var arithmetic = {
+    
+    plus: function(){
+    
+    },
+    
+    minus: function(){
+
+    },
+
+    divide: function(){
+
+    },
+    times: function(){
+
+    }
+};
+
+var funcs = {
+
+    posNeg: function(){
+
+    },
+
+    percentage: function(){
+
+    },
+
+    clear: function(){
+
+    }
+
+};
+
+var handlers = {
+
+}
